aa4750a425245a837de0751581db8cb1b414abc0
suheyon4820 <bananacco@naver.com>
2020-02-16 23:37:30 +0900

    upload

---
+++ keras27_cifar10_2_dnn.py
@@ -0,0 +1,42 @@
+from keras.datasets import cifar10
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, LSTM
+from keras.datasets import mnist
+from keras.callbacks import EarlyStopping
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+print(x_train.shape) #(50000, 32, 32, 3)
+print(x_test.shape) #(10000, 32, 32, 3)
+print(y_train.shape) #(50000, 1)
+
+x_train = x_train.reshape(x_train.shape[0], 32*32*3)
+x_test = x_test.reshape(x_test.shape[0], 32*32*3)
+print(x_train.shape) #(500000, 3072)
+print(x_test.shape) #(500000, 3072)
+
+from keras.utils import np_utils
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+print(y_train.shape) #(50000, 10)
+print(y_test.shape) #(10000, 10)
+
+model = Sequential()
+model.add(Dense(256, input_shape=(3072, ))) 
+model.add(Dense(256, activation = 'relu'))
+model.add(Dense(256, activation = 'relu'))
+model.add(Dense(256, activation = 'relu'))
+model.add(Dense(256, activation = 'relu'))
+model.add(Dense(10, activation = 'softmax'))
+
+model.summary()
+
+# compile
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) # metrics = accuracy(딱 떨어지는 값에 쓰임)
+early_stopping = EarlyStopping(monitor = 'loss', patience=20)
+model.fit(x_train, y_train, validation_split=0.7, epochs=100, batch_size=8, verbose=1, callbacks=[early_stopping])
+
+acc = model.evaluate(x_test, y_test)
+print(acc)
